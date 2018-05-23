69e65332999c3b1257d661a84074dd7d0352c2a4
IOpsycode <acollmus@gmail.com>
2018-05-23 14:06:29 -0500

    prophet function
    wrote function to quickly show prophet on various columns. Unfortunately had a crash and lost the ARMA/ARIMA work done previously

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GraphicForecastingOptions
+
+## Purpose
+The purpose of this project is to test/demonstrate forecasting capabilities
+
+1. test the fbprophet program
+2. test ARMA and ARIMA from statsmodels
+3. focus on Austin site
+4. expand to other sites and other slices
+5. if necessary, pilot more advanced models (e.g. ARIMAX)
--- attempt.py
+++ attempt.py
@@ -3,6 +3,37 @@
 Created on Fri May 18 13:00:50 2018
 
 @author: auracoll
-"""
 
+Goal for now is to show site level and country level. Maybe an example by Grade
+
+"""
+import pandas as pd
+import numpy as np
 from fbprophet import Prophet
+
+
+# Data import and cleaning
+data = pd.read_csv('Data/April18.csv', skiprows=1)
+
+grouped = data.groupby('Report Date').sum()
+
+# something weird happened here, but just del for now
+grouped.loc['10/17/2017',] = np.nan
+
+def forecast(column, pd='M', n_pds=12):
+    # forecast a given column with pd=period unit and n_pds=future pds to forecast 
+    
+    newdf = grouped[column].reset_index()
+    newdf.columns = ['ds', 'y']
+    m = Prophet().fit(newdf)
+    future = m.make_future_dataframe(freq=pd, periods=n_pds)
+    fcst = m.predict(future)
+    grph = m.plot(fcst)
+    return grph
+    
+column = 'Headcount'
+grouped[column].reset_index()
+forecast('Headcount')
+forecast('Leavers Voluntary')
+forecast('Leavers Involuntary')
+forecast('New Openings')
